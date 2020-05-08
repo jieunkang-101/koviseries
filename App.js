e90fbb3c87e82a43e1bcfcd537ee64a5f3feef72
Jieun Kang <jieunkang101@gmail.com>
2020-05-08 12:40:03 -0700

    add cacheResourcesAsync function with cacheFonts

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
-import { Image, Text } from "react-native";
+import { Text, Image, View } from "react-native";
 import { AppLoading } from 'expo';
-
-
-const cacheImages = (images) => images.map(image => {
-  if (typeof image === "string") {
-    return Image.prefetch(image);
-  } else {
-    return Asset.fromModule(image).downloadAsync();
-  }
-});
-
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = () => {
-    const images = cacheImages([
-      "https://unsplash.com/photos/_0UgNXhFzT8",
+
+  const cacheResourcesAsync = () => {
+    const images = [
+      "https://images.unsplash.com/photo-1588892862675-802c046d2ed9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       require("./assets/splash.jpg")
-    ]);
-    return Promise.all([...images]);
+    ];
+    const cacheImages = images.map(image => {
+      if (typeof image === "string") {
+        return Image.prefetch(image);
+      } else {
+        return Asset.fromModule(image).downloadAsync();
+      }
+    });
+
+    const fonts = [Ionicons.font];
+    const cacheFonts = fonts.map(font => Font.loadAsync(font));
+    return Promise.all([cacheImages, cacheFonts]);
   }  
 
   const onFinish = () => setIsReady(true);
-  return isReady ? null : (
+
+  return isReady ? (
+    <View style={{ flex: 1 }}>
+      <Image source={require('./assets/splash.jpg')} />
+    </View>
+  ) : (
     <AppLoading   
-      startAsync={loadAssets} 
+      startAsync={cacheResourcesAsync} 
       onFinish={onFinish} 
       onError={console.error} 
     />
